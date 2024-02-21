88712685768151ac6671322f99c21dce3adfef66
MinutePapillon <christophebarrere@gmail.com>
2024-02-21 12:53:17 +0000

    ee

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -11,7 +12,7 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for (low = 'a'; low <= ''; low++)
 		putchar(low);
 		putchar('\n');
 
