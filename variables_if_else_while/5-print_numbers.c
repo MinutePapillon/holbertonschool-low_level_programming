bac73b09a229a659b6f070507997cc63f7d3843f
MinutePapillon <christophebarrere@gmail.com>
2024-02-21 15:13:02 +0000

    ee

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,14 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int c;
-	
-	for (c = '0'; c <= '9'; c++)
-		putchar(c);
-		putchar('\n');
-	return(0);
-}
+	int i;
 
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
