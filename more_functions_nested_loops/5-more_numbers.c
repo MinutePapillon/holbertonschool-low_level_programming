08cbe1038cf619142ec238f78f18b0ab278df720
MinutePapillon <christophebarrere@gmail.com>
2024-02-26 12:56:07 +0000

    ee

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers- function that prints the numbers, from 0 to 14
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '14'; i++)
+	{
+		_putchar (i);
+	}
+		_putchar ('\n');
+}
+
