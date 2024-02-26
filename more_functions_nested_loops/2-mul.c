827e2efdeabe26109fd39614e2a027870680c74d
MinutePapillon <christophebarrere@gmail.com>
2024-02-26 10:50:38 +0000

    ee

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a : imput digit
+ * @b : imput digit
+ * Return: mul(int a, int b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
