328fd51afea32b0584d34186c692d8da4e290be8
MinutePapillon <christophebarrere@gmail.com>
2024-02-27 13:05:12 +0000

    e

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 
 /**
- * 
- * 
+ * main - prints Fizz
+ *
+ * Return: 0
  */
 
-int main()
+int main(void)
 {
-	int i = 1;
+int i = 1;
 
-	while (i <= 100)
-	{
-			if (i % 3 == 0 && i % 5 == 0)
-			{	
-				printf("FizzBuzz");
-				if (i < 100)
-                                        printf(" ");
-			}
-			else if (i % 5 == 0)
-                	{
-                        	printf("Buzz");
-				if (i < 100)
-                                        printf(" ");
-                	}
-			else if (i % 3 == 0)
-                	{
-                        	printf("Fizz");
-				if (i < 100)
-                                        printf(" ");
-                	}
-			else 
-			{
-				printf("%d", i);
-				if (i < 100)
-					printf(" ");
-			}
-	i++;
-	}
-	printf("\n");
-	return (0);
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+if (i < 100)
+printf(" ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+if (i < 100)
+printf(" ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz");
+if (i < 100)
+printf(" ");
+}
+else
+{
+printf("%d", i);
+if (i < 100)
+printf(" ");
+}
+i++;
+}
+printf("\n");
+return (0);
 }
 
