a9994dab5459511cfc8ab1b1cf023c5b0ecad5ef
MinutePapillon <christophebarrere@gmail.com>
2024-02-27 12:47:18 +0000

    e

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,30 @@ int main()
 
 	while (i <= 100)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 5 == 0)
-                {
-                        printf("Buzz");
-                }
-		else if (i % 3 == 0)
-                {
-                        printf("Fizz");
-                }
-		else 
-		{
-			printf("%d", i);
-
-			if (i < 100)
-				printf(" ");
-		}
+			if (i % 3 == 0 && i % 5 == 0)
+			{	
+				printf("FizzBuzz");
+				if (i < 100)
+                                        printf(" ");
+			}
+			else if (i % 5 == 0)
+                	{
+                        	printf("Buzz");
+				if (i < 100)
+                                        printf(" ");
+                	}
+			else if (i % 3 == 0)
+                	{
+                        	printf("Fizz");
+				if (i < 100)
+                                        printf(" ");
+                	}
+			else 
+			{
+				printf("%d", i);
+				if (i < 100)
+					printf(" ");
+			}
 	i++;
 	}
 	printf("\n");
