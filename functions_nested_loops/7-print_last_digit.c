30319efb7b2f9267133ecec6ff337a8eea5fb674
MinutePapillon <christophebarrere@gmail.com>
2025-02-21 14:01:35 +0100

    Write a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Function prints the last digit of a number
+ *
+ * @n: nombre a tester
+ * Return: return the resultat
+ */
+int print_last_digit(int n)
+{
+	int c;
+	c = n % 10;
+	_putchar(c);
+	return (0);
+}
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * @a: hkjhkj
+ * @b: hkjuhkjh
+ * @c: jhkjhk
+ * @d: ghjjhgjh
+ * Return: 0 on sucess
+ */
+void jack_bauer(void)
+{
+	char a, b, c, d;
+
+	for (a = '0'; a <= '1'; a++)
+	{
+		for (b = '0';b <= '9'; b++)
+		{
+			for (c = '0';c <= '5'; c++)
+			{
+				for (d = '0'; d <= '9'; d++ )
+				{
+				_putchar(a);
+				_putchar(b);
+				_putchar(':');
+				_putchar(c);
+				_putchar(d);
+				_putchar('\n');
+				}
+			d = '0';
+			}
+		c = '0';
+		}
+	b = '0';
+	}
+
+	
+	for (a = '1'; a <= '2'; a++)
+	{
+		for (b = '0';b <= '3'; b++)
+		{
+			for (c = '0';c <= '5'; c++)
+			{
+				for (d = '0'; d <= '9'; d++ )
+				{
+				_putchar(a);
+				_putchar(b);
+				_putchar(':');
+				_putchar(c);
+				_putchar(d);
+				_putchar('\n');
+				}
+			d = '0';
+			}
+		c = '0';
+		}
+	b = '0';
+	}
+}
