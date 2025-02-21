21b501a3bdc84e05dd9c3a5da9aecd4e1753ecf9
MinutePapillon <christophebarrere@gmail.com>
2025-02-21 17:05:17 +0100

    Write a function that adds two integers and returns the result

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Function that adds two integers and returns the result
+ *
+ * @a: nombre a tester
+ * @b: nombre a tester
+ * Return: return the resultat
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * time_table - Function that prints the 9 times table, starting with 0
+ *
+ * @n: nombre a tester
+ * Return: return always 0
+ */
+void times_table(void)
+{
+	int a, b;
+	for (a =
+}
