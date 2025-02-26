4a185e619683fa8d7f6d9353e2a7b395716a57f0
MinutePapillon <christophebarrere@gmail.com>
2025-02-26 19:42:05 +0100

    Write a function that copies the string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies the string
+ * @dest: pointeur
+ * @src: pointeur
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i + 1] = '\0';
+	return (src);
+}
