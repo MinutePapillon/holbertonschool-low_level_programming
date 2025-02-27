65e25073f367e5ead40d187f59a7918e9597aaec
MinutePapillon <christophebarrere@gmail.com>
2025-02-27 13:37:48 +0100

    Write a function that concatenates two strings.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+/**
+ * _atoi - function that convert a string to an integer
+ * @s: pointeur
+ *
+ * Return: always 0
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] >= '0') && (s[i] <= '9'))
+				return (s[i]);
+	}
+	return ()
+}
