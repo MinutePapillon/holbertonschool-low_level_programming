e396170f1dec9236aeae2027fda3125946b6d9e0
MinutePapillon <christophebarrere@gmail.com>
2025-03-05 21:33:08 +0100

    Write a function that returns the factorial of a given number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,15 @@
+/**
+ * factorial - function that returns the factorial of a given number
+ * @n: number
+ *
+ * Return: return the factorial of a given number
+ */
+int factorial(int n)
+{
+	int result;
+
+	if (n <= 0)
+		return (1);
+	result = n * factorial(n-1);
+	return (result);
+}
