53fc70cd8797571bf86aa220bc277b71b9ba31c1
MinutePapillon <christophebarrere@gmail.com>
2025-03-06 15:39:46 +0100

    Write a program that multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -8,17 +8,19 @@
  *
  * Return: always 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
+	int a;
+	int b;
 
-	if (argv[1] == NULL || argv[2] == NULL)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
 	printf("%d\n", a * b);
 
 	return (0);
