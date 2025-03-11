ef06fbf2bb2ac9d0eff8bb8afc5053479bf2ad11
MinutePapillon <christophebarrere@gmail.com>
2025-03-11 13:29:08 +0100

    Write a function that concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,49 +14,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *p;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-		i = 0;
-	}
-	else
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			;
-	}
-
 	if (s2 == NULL)
-	{
 		s2 = "";
-		j = 0;
-	}
-	else
-	{
-		for (j = 0; s2[j] != '\0'; j++)
-			;
-	}
 
-	if (n < j)
-		p = malloc(sizeof(char) * (i + n + 1));
-	else
-		p = malloc(sizeof(char) * (i + j + 1));
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	if (n > j)
+		n = j;
+
+	p = malloc(sizeof(char) * (i + n + 1));
 
 	if (p == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
 		p[k] = s1[k];
 
-	if (n < j)
-	{
-		for (l = 0; l < n; l++)
-			p[l + i] = s2[l];
-	}
-	else
-	{
-		for (l = 0; l < j; l++)
-			p[l + i] = s2[l];
-	}
+	for (l = 0; l < n; l++)
+		p[k + l] = s2[l];
 
-	p[l + i + 1] = '\0';
+	p[l + k] = '\0';
 
 	return (p);
 }
