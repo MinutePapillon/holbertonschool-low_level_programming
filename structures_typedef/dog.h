20fd884a5e3c42a1bb0f8738d4ae74f83ac1319b
MinutePapillon <christophebarrere@gmail.com>
2025-03-12 23:17:38 +0100

    Define a new type struct dog with the following element

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - function that initialize a variable of type struct dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
