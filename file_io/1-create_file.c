7680bc80c152a9e6434a2b8b93f8076b3d51f065
MinutePapillon <christophebarrere@gmail.com>
2025-04-04 16:57:19 +0200

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (0);
 	}
 
-	if (text_content == NULL)
+	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
 			len++;
