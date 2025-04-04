17ea765b45ae5a56d75146f1cca96beaf8196011
MinutePapillon <christophebarrere@gmail.com>
2025-04-04 14:17:42 +0200

    Create a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
-	ssize_t  n_written;
+	int n_written;
 
 	if (filename == NULL)
 		return (-1);
@@ -36,8 +36,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fd);
-	if (close(fd) == -1)
-		return (-1);
 
 	return (1);
 }
