07fe52d63ea32b30eb1ec5e6fe3bd495eceeed4f
MinutePapillon <christophebarrere@gmail.com>
2025-04-04 12:32:20 +0200

    feat: add octet to buffer

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t n_read, n_written;
-	char buffer[1024];
+	char buffer[2048];
 
 	if (filename == NULL)
 		return (0);
